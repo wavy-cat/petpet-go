75981d5007ca10b006fcdca029d62fa6bcd76ef4
wavy-cat <52543076+wavy-cat@users.noreply.github.com>
2024-09-16 21:26:42 +0500

    And PORT to env and workflow for GHCR

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,5 +7,7 @@ var HTTPAddress = ":80"
 func init() {
 	if env := os.Getenv("ADDRESS"); env != "" {
 		HTTPAddress = env
+	} else if env := os.Getenv("PORT"); env != "" {
+		HTTPAddress = ":" + env
 	}
 }
