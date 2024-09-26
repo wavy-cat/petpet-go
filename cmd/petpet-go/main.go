3935ba1378ecbff3f5979e3ad5a1cbcf51173dc8
wavy-cat <52543076+wavy-cat@users.noreply.github.com>
2024-09-26 17:15:39 +0500

    Update index page

--- cmd/petpet-go/main.go
+++ cmd/petpet-go/main.go
@@ -5,7 +5,8 @@ import (
 	"errors"
 	"fmt"
 	"github.com/gorilla/mux"
-	"github.com/wavy-cat/petpet-go/http/handler"
+	"github.com/wavy-cat/petpet-go/http/handler/index"
+	"github.com/wavy-cat/petpet-go/http/handler/petpet"
 	"github.com/wavy-cat/petpet-go/http/middleware"
 	"github.com/wavy-cat/petpet-go/internal/config"
 	"github.com/wavy-cat/petpet-go/pkg/discord"
@@ -43,19 +44,16 @@ func main() {
 	handle := middleware.Essentials{
 		Next: &middleware.Logging{
 			Logger: logger,
-			Next:   handler.Handler{},
+			Next:   petpet.Handler{},
 		},
 		Objects: objects,
 	}
 	router.Handle("/ds/{user_id}.gif", &handle).Methods(http.MethodGet)
 	router.Handle("/ds/{user_id}", &handle).Methods(http.MethodGet)
-
-	router.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-		_, err := w.Write([]byte("Ты думал тут что-то будет?"))
-		if err != nil {
-			logger.Error("Error sending response", zap.Error(err))
-		}
-	})
+	router.Handle("/", &middleware.Logging{
+		Logger: logger,
+		Next:   index.Handler{},
+	}).Methods(http.MethodGet, http.MethodHead)
 
 	// Настраиваем сервер
 	srv := &http.Server{
