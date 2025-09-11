7a827ee063a924860821edd4c5e61cdc2832be3e
WavyCat <me@wavycat.ru>
2025-09-11 18:54:01 +0500

    change user agent

--- pkg/discord/types.go
+++ pkg/discord/types.go
@@ -4,7 +4,7 @@ const baseURL = "https://discord.com/api/v10"
 
 const baseCDNURL = "https://cdn.discordapp.com"
 
-const userAgent = "PetPetBot/1.0"
+const userAgent = "PetPet-Go Discord Library/1.0"
 
 type ctxKeyTransport int
 
