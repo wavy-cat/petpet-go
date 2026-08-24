46827add6b2b279a136f9f2f9aa9d7fe607b0a05
WavyCat <root@wavycat.me>
2026-08-24 14:42:07 +0500

    feat(middleware): support Cf-Ray header for request ID

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -24,6 +25,9 @@ const RequestIDKey ctxKeyRequestID = 0
 // RequestIDHeader is the name of the HTTP Header which contains the request id
 const RequestIDHeader = "X-Request-ID"
 
+// RequestCfRayHeader is the name of the HTTP Header which contains the request id specify to Cloudflare reverse proxy
+const RequestCfRayHeader = "Cf-Ray"
+
 // LoggerFromContext returns the request logger stored by Logger.
 func LoggerFromContext(ctx context.Context) (*zap.Logger, bool) {
 	logger, ok := ctx.Value(LoggerKey).(*zap.Logger)
@@ -35,18 +39,12 @@ func Logger(logger *zap.Logger, service string) func(next http.Handler) http.Han
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			ctx := r.Context()
 
-			requestID := r.Header.Get(RequestIDHeader)
-			if requestID == "" {
-				var err error
-				requestID, err = gonanoid.New()
-				if err != nil {
-					logger.Fatal("Failed to generate request ID", zap.Error(err))
-				}
+			requestID, err := getRequestID(r)
+			if err != nil {
+				logger.Fatal("Failed to generate request ID", zap.Error(err))
 			}
 
-			logger := logger.
-				With(zap.String("requestId", requestID)).
-				With(zap.String("service", service))
+			logger := logger.With(zap.String("requestId", requestID))
 
 			ctx = context.WithValue(ctx, RequestIDKey, requestID)
 			ctx = context.WithValue(ctx, LoggerKey, logger)
@@ -72,4 +70,22 @@ func Logger(logger *zap.Logger, service string) func(next http.Handler) http.Han
 			next.ServeHTTP(ww, r.WithContext(ctx))
 		})
 	}
+}
+
+func getRequestID(r *http.Request) (string, error) {
+	// header X-Request-ID
+	if requestID := r.Header.Get(RequestIDHeader); requestID != "" {
+		return requestID, nil
+	}
+
+	// header Cf-Ray
+	if requestID := r.Header.Get(RequestCfRayHeader); requestID != "" {
+		return requestID, nil
+	}
+
+	requestID, err := gonanoid.New()
+	if err != nil {
+		return "", fmt.Errorf("error when generate request id: %w", err)
+	}
+	return requestID, nil
 }
